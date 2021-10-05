2cc8c5efad39832e847548c4e147c875783cfc84
Anddy Lopez <anddylopez5324@gmail.com>
2021-10-05 09:30:27 -0500

    First commit

---
+++ hello.sh
@@ -0,0 +1,46 @@
+W# This is a comment to add more information to your code
+# echo Comment
+echo Hello # <- This is an echo to display Hello
+echo World
+
+msg="Hello World"
+
+echo $msg
+
+echo What is your name?
+read userInput
+echo "Hello $userInput, Welcome to Programming!"
+
+# Control Flow
+# Types of loops
+# For loop - They will repeat something a set number of times
+for var in 1 2 3 4 5
+do
+	echo $var
+	echo This variable currently holds: $var
+done
+
+for i in {1..10..1} #{starting number.. end number ..increment number}
+do
+	echo $i
+done
+
+# While loops - they will repeat something until the condtion isn't satisfied anymore
+condition="true"
+
+while [ "$condition" != "false" ]
+do
+	echo "Do you want to repeat? (true or false)"
+	read condition
+	echo "You typed: $condition"
+done
+
+# If loops
+
+echo "What is 2 + 2?"
+read input
+if [ "$input" == "4" ]
+then
+	echo You are smart
+fi
+
