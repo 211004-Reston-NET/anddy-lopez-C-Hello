cdb105e5766a0586e8800708be5a8f17eeb8e404
AnddyLopez <anddylopez5324@gmail.com>
2021-10-06 15:23:04 -0500

    P0 Practice

---
+++ C#_Fundamentals/RestaurantReview/RRModels/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RRModels
+{
+    public class Restaurant
+    {
+        private string _city;
+        public string City
+        {
+            get { return _city; }
+            set 
+            {
+                //Main idea - this Regex will find me any number inside of my string
+                if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                {
+                    //Will give the user an exception whenever you try to set the city field with a number
+                    throw new Exception("City can only hold letters!"); 
+                }
+                _city = value;
+            }
+        }
+        
+
+        public string State { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ C#_Fundamentals/RestaurantReview/RRUI/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RRUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+                //Clean the screen on the terminal
+                Console.Clear();
+                Console.WriteLine("Welcome to the Restaurant");
+                string userChoice;
+                Console.WriteLine("[1] - Do you want to clear the screen?");
+                Console.WriteLine("[2] - Do you want to exit out of the program?");
+                userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "1":
+                        Console.WriteLine("User has chosen to clear the screen");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        repeat = false;
+                        Console.WriteLine("YOu have exited to the program!");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("What did you just type, try again!");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
